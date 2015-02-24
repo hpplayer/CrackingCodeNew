1c486356212a316de210265f42e778888d9a5565
hpPlayer <hpplayer@live.cn>
2015-02-24 00:23:37 -0600

    Finished p_14_05

---
+++ src/Chapter14/Rectangle.java
@@ -0,0 +1,14 @@
+package Chapter14;
+
+public class Rectangle {
+	private double width;
+	private double height;
+	public Rectangle(double w, double h){
+		width = w;
+		height = h;
+	}
+	
+	public double area(){
+		return width * height;
+	}
+}
